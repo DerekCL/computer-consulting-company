e988efc966a57e486d0f1faaa24102ee1dfaab97
Derek Lewandowski <derekclewandowski@gmail.com>
2017-12-10 16:32:47 -0500

    remote support page initilization

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router.get('/', (req, res) => res.render("index"))
 router.get('/services', (req, res) => res.render("services"))
 router.get('/contact', (req, res) => res.render("contact"))
+router.get('/contact/remote_support', (req, res) => res.render("remote_support"))
+
+
 
 module.exports = router;
