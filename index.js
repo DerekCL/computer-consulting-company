745a8a9b08511460ea51d09e466cc4829038d38b
Derek Lewandowski <derekclewandowski@gmail.com>
2017-12-10 00:48:46 -0500

    setup basic app libraries

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ require("dotenv").config();
 // File Initialization
 let express = require("express");
 let path = require("path");
+let favicon = require("serve-favicon");
+let logger = require("morgan");
+let bodyParser = require("body-parser");
 
 // Express initiation
 let app = express();
+// View Engine initilization
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+// Logger 
+app.use(logger("dev"));
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Static Content
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon", "favicon.ico")));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
