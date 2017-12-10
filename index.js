c06b524cdf3191a043f46f64ecc08af743a76f5c
Derek Lewandowski <derekclewandowski@gmail.com>
2017-12-10 13:10:07 -0500

    setup index to be more modular

--- index.js
+++ index.js
@@ -14,21 +14,29 @@ let bodyParser = require("body-parser");
 
 // Express initiation
 let app = express();
+
 // View Engine initilization
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 // Logger 
 app.use(logger("dev"));
+
 // Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // Static Content
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon", "favicon.ico")));
-
 
 // Routing Setup
 const routes = require("./routes/index");
 app.use("/", routes);
 
-app.listen(3000, () => console.log('server started on http://localhost:3000'))
+//Port Setup
+let port = process.env.PORT || '8000';
+app.set('port', port);
+
+//Server Starting
+app.listen(port, () => console.log('server started on http://localhost:'+ port))
