d81f91d91bf5c3a56047ad4e484fd3f96ee60091
Derek Lewandowski <derekclewandowski@gmail.com>
2017-12-10 15:53:44 -0500

    setup routing to the new pages

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => res.render("index"))
+router.get('/services', (req, res) => res.render("services"))
+router.get('/contact', (req, res) => res.render("contact"))
 
 module.exports = router;
